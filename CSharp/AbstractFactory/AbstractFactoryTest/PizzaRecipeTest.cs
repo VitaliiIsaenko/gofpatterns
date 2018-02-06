782cd81f1acaab8a2f191cb9cca634d5b9ca0701
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-02-06 14:35:38 +0100

    Add tests for pizza ingredients

---
+++ CSharp/AbstractFactory/AbstractFactoryTest/PizzaRecipeTest.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using AbstractFactory;
+
+
+namespace AbstractFactoryTest {
+    public class PizzaRecipeTest {
+        [Fact]
+        public void GetMargaritaCheese() {
+            //Arrange
+            IPizzaRecipe pizzaRecipe = new MargaritaRecipe();
+
+            //Action
+            ICheese cheese = pizzaRecipe.GetCheese();
+
+            //Assert
+
+            Assert.Equal("Margarita cheese",cheese.GetName());
+        }
+
+        [Fact]
+        public void GetPepperoniCheese() {
+            //Arrange
+            IPizzaRecipe pizzaRecipe = new PepperoniRecipe();
+
+            //Action
+            ICheese cheese = pizzaRecipe.GetCheese();
+
+            //Assert
+
+            Assert.Equal("Pepperoni cheese",cheese.GetName());
+        }
+    }
+}
