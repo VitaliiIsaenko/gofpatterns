f0c62c1936ed8f3200aa47542d3cc73f87cc302b
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-02-06 14:37:19 +0100

    Add tests for Cook class

---
+++ CSharp/AbstractFactory/AbstractFactoryTest/CookTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace AbstractFactoryTest
+{
+    public class CookTest   
+    {
+        // [Fact]
+        // public void PreparePizza()
+        // {
+        //     //Arrange
+        //     Cook cook = new Cook();
+        //     IPizzaRecipe margaritaRecipe = new MargaritaRecipe();
+
+        //     //Action
+        //     IPizza pizza = cook.preparePizza(margaritaRecipe);
+            
+        //     //Assert
+        //     Assert.Equal(pizza.getCheese(), "Margarita cheese");
+        // }
+
+
+    }
+}
