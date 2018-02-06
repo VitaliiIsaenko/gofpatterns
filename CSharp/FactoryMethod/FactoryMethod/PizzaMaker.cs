9f518b201005adb27ddd02269bc12e333a76a59d
Vitalii Isaenko <lookyisaenko@gmail.com>
2018-02-06 12:55:52 +0100

    Add a class with a factory method

---
+++ CSharp/FactoryMethod/FactoryMethod/PizzaMaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public abstract class PizzaMaker
+    {
+        public string GetPizzaIngredients() {
+            IPizza pizza = CreatePizza();
+            return pizza.GetIngredients();
+        }
+
+        public abstract IPizza CreatePizza();
+    }
+}
